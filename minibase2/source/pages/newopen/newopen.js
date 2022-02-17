4492f271cb0ed00e5d194c533f5217a5d273e815
xiaogu-zhaozhao <1607634573@qq.com>
2022-02-17 18:02:08 +0800

    20220217

---
+++ minibase2/source/pages/newopen/newopen.js
@@ -0,0 +1,26 @@
+// pages/content/content.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+    this.Base.setMyData({
+        url:this.Base.options.url
+      })
+  }
+  onMyShow() {
+    var that = this;
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad; 
+body.onMyShow = content.onMyShow;
+Page(body)
